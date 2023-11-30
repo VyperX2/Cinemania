8cf03a70c5ed5e8129a3d5a83a129a5e5469cdc6
VyperX2 <mateenahmed.qatar@gmail.com>
2023-11-30 14:16:26 +0300

    Display genres and get cast

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -6,6 +6,14 @@ const MoviePage = async ({ params }: { params: { id: number } }) => {
 		`https://api.themoviedb.org/3/movie/${params.id}?api_key=${process.env.TMDB_API_KEY}`
 	);
 	const data: Data = await response.json();
+
+	const res = await fetch(
+		`https://api.themoviedb.org/3/movie/${params.id}/credits?api_key=${process.env.TMDB_API_KEY}`
+	);
+	const { cast } = await res.json();
+
+	console.log(cast);
+	const actors = cast.slice(0, 6);
 	return (
 		<section className="mt-20 flex justify-center container">
 			<CurrentMovie {...data} />
--- components/CurrentMovie.tsx
+++ components/CurrentMovie.tsx
@@ -15,6 +15,19 @@ const CurrentMovie = ({
 	vote_count,
 	genres,
 }: Data) => {
+	console.log({
+		backdrop_path,
+		id,
+		original_language,
+		original_title,
+		overview,
+		poster_path,
+		release_date,
+		title,
+		vote_average,
+		vote_count,
+		genres,
+	});
 	return (
 		<section className="grid grid-cols-1 lg:grid-cols-2 w-full place-items-center mt-8">
 			<div className="relative sm:h-[550px] sm:w-[384px] w-[295px] h-[470px] rounded-2xl overflow-hidden   shadow-2xl shadow-black dark:shadow-gray-600  ">
@@ -25,13 +38,21 @@ const CurrentMovie = ({
 					objectFit="cover"
 				/>
 			</div>
-			<div className="flex flex-col gap-y-8 items-center dark:text-white mt-10 lg:mt-0 md:mx-0">
-				<h1 className="lg:text-5xl text-3xl md:text-4xl font-semibold">
+			<div className="flex flex-col gap-y-8 items-center lg:-mt-20 mt-10 dark:text-white">
+				<h1 className="lg:text-4xl text-2xl md:text-3xl font-semibold">
 					{original_title}
 					<span> ({release_date.substring(0, 4)})</span>
 				</h1>
-				<h2 className=" text-4xl">Overview</h2>
-				<p className=" text-2xl">{overview}</p>
+				<p>Rating : {vote_average}</p>
+				<div className="flex items-center justify-around w-full">
+					{genres.map((genre) => (
+						<p className="font-semibold">{genre.name}</p>
+					))}
+				</div>
+				<div>
+					<h2 className=" text-xl">Overview</h2>
+					<p className=" text-md">{overview}</p>
+				</div>
 				<div className="flex justify-between w-full">
 					<h4>
 						Language :{" "}
