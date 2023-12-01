2fb7389a858dc61885d0d8fe90f99049de8aaf32
VyperX2 <mateenahmed.qatar@gmail.com>
2023-12-01 14:06:16 +0300

    Add animations to MovieCards

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -8,8 +8,8 @@ const Feed = ({ data }: FeedProps) => {
 	return (
 		<section className="flex flex-col container">
 			<div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 place-items-center">
-				{data?.map((movie) => (
-					<MovieCard key={movie.id} {...movie} />
+				{data?.map((movie, index) => (
+					<MovieCard key={movie.id} i={index} {...movie} />
 				))}
 			</div>
 		</section>
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,12 +2,29 @@
 import { Data } from "@/types/Data";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { animate, motion } from "framer-motion";
 
-const MovieCard = ({ id, poster_path, title }: Data) => {
+const MovieCard = ({ id, poster_path, title, i }: Data & { i: number }) => {
+	setTimeout(() => window.scrollTo(0, 0), 0);
+	// Scale property was causing the window to scroll to the center
 	const router = useRouter();
 	// Todo Fix the title wrap
+	const fadeInAnimationVariants = {
+		initial: { opacity: 0, scale: 0 },
+		animate: {
+			opacity: 100,
+			scale: 1,
+			transition: { delay: i * 0.05, ease: "easeInOut" },
+		},
+	};
+	// TODO Animate on scroll and remove the delay for scroll
+
 	return (
-		<article
+		<motion.article
+			variants={fadeInAnimationVariants}
+			initial="initial"
+			animate="animate"
+			viewport={{ once: true }}
 			onClick={() => router.push(`/movie/${id}`)}
 			className="flex flex-col w-60 items-center py-4 gap-2 overflow-hidden max-h-[450px]"
 		>
@@ -21,7 +38,7 @@ const MovieCard = ({ id, poster_path, title }: Data) => {
 			<h4 className=" text-xl text-center whitespace-nowrap">
 				{title.substring(0, 27)}
 			</h4>
-		</article>
+		</motion.article>
 	);
 };
 
