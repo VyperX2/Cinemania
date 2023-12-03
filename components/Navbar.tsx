d473493ca91bc0b5e0c38b1045a343cf26e7f04a
VyperX2 <mateenahmed.qatar@gmail.com>
2023-12-03 16:47:16 +0300

    Add Site name to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,8 +6,10 @@ const Navbar = () => {
 	const [value, setValue] = useState<string>("");
 	const router = useRouter();
 	return (
-		<nav className="fixed w-full top-0 h-20 bg-secondary flex items-center font-body z-10">
-			<p className=" ml-4"></p>
+		<nav className="fixed w-full top-0 h-20 gap-4 bg-secondary flex items-center font-body z-10 px-6">
+			<h4 onClick={() => router.push("/")} className="  text-xl cursor-pointer">
+				CINEMANIA
+			</h4>
 			<form
 				onSubmit={(e) => {
 					e.preventDefault();
@@ -31,10 +33,23 @@ const Navbar = () => {
 						setValue(e.target.value)
 					}
 					type="text"
-					className=" bg-transparent  w-full outline-none placeholder:text-text placeholder:font-body placeholder:text-md transition-all -mb-1"
+					className=" bg-transparent w-full outline-none placeholder:text-text placeholder:font-body placeholder:text-md transition-all -mb-1"
 					placeholder="Search for a movie..."
 				/>
 			</form>
+			<button className="">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+				>
+					<path
+						fill="currentColor"
+						d="M12 18V6a6 6 0 0 1 6 6a6 6 0 0 1-6 6m8-2.69L23.31 12L20 8.69V4h-4.69L12 .69L8.69 4H4v4.69L.69 12L4 15.31V20h4.69L12 23.31L15.31 20H20z"
+					/>
+				</svg>
+			</button>
 		</nav>
 	);
 };
