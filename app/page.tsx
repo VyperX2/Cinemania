b1fce06b9832e0225bc10f5affc40da7ae92f0fd
VyperX2 <mateenahmed.qatar@gmail.com>
2023-12-07 22:03:02 +0300

    Display Poster

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
 import { Data } from "@/types/Data";
 import Feed from "@/components/Feed";
+import MoviePoster from "@/components/MoviePoster";
 
 export default async function Home() {
 	const response = await fetch(
-		`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}` , {cache : "no-store"}
+		`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}`,
+		{ cache: "no-store" }
 	);
 	const results = await response.json();
 	const data: Data[] = await results.results;
+	const firstMovie = data[0];
+
+	const remainingMovies = data.filter(
+		(movie) => JSON.stringify(movie) !== JSON.stringify(firstMovie)
+	);
 
 	return (
 		<main className="mt-20">
-			<Feed data={data} />
+			<MoviePoster posterMovie={firstMovie} />
+			<Feed data={remainingMovies} />
 		</main>
 	);
 }
---
+++ components/MoviePoster.tsx
@@ -0,0 +1,25 @@
+const MoviePoster = ({ posterMovie }) => {
+	return (
+		<div className="container">
+			<div
+				style={{
+					backgroundImage: `url(https://image.tmdb.org/t/p/original//${posterMovie.backdrop_path})`,
+				}}
+				alt="poster"
+				className=" w-full md:h-[450px] h-64 bg-cover bg-center rounded-md relative cursor-pointer poster overflow-hidden "
+			>
+				<div className=" absolute w-full h-full bg-black/30" />
+				<div className=" absolute bottom-3 flex flex-col left-3 ">
+					<h4 className=" text-lg md:text-3xl text-white">
+						{posterMovie.original_title}
+					</h4>
+					<h4 className=" text-sm md:w-[50%] w-full text-white">
+						{posterMovie.overview}
+					</h4>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default MoviePoster;
