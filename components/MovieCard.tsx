73b087d30acc25ef134594401c62c4d018acaab0
VyperX2 <mateenahmed.qatar@gmail.com>
2023-12-16 20:47:41 +0300

    Fix Movie Titles

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,18 +2,16 @@
 import { Data } from "@/types/Data";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { easeIn, motion } from "framer-motion"
+import { easeIn, motion } from "framer-motion";
 
 const MovieCard = ({ id, poster_path, title, i }: Data & { i: number }) => {
-	
-	
 	const router = useRouter();
 	// Todo Fix the title wrap
 
 	return (
 		<motion.article
-      initial={{opacity : 0 , scale : 0}}
-      animate={{opacity : 1 , scale : 1 , transition : { ease : easeIn}}}
+			initial={{ opacity: 0, scale: 0 }}
+			animate={{ opacity: 1, scale: 1, transition: { ease: easeIn } }}
 			onClick={() => router.push(`/movie/${id}`)}
 			className="flex flex-col w-60 items-center py-4 gap-2 overflow-hidden max-h-[450px]"
 		>
@@ -24,8 +22,8 @@ const MovieCard = ({ id, poster_path, title, i }: Data & { i: number }) => {
 				height={311}
 				className=" w-52 object-cover rounded-lg hover:scale-[1.05] mb-1 cursor-pointer transition-all"
 			/>
-			<h4 className=" text-xl text-center whitespace-nowrap">
-				{title.substring(0, 27)}
+			<h4 className=" text-xl text-center">
+				{title.length > 18 ? `${title.substring(0, 18)}...` : title}
 			</h4>
 		</motion.article>
 	);
